9de45b37ec4b4a1cd8ec3ba90a2e7a7a031b3c44
James Shields <james@lostcarpark.com>
2022-01-09 13:06:36 +0000

    Added filtering by location using react-select.

--- src/components/FilterableProgram.js
+++ src/components/FilterableProgram.js
@@ -1,40 +1,75 @@
 import React, { useState } from "react";
+import ReactSelect from "react-select";
 import ProgramList from "./ProgramList";
 
-const FilterableProgram = ({ program, handler }) => {
-  const [filtered, setFiltered] = useState(program);
+const FilterableProgram = ({ program, locations, handler }) => {
+  const [search, setSearch] = useState("");
+  const [selLoc, setSelLoc] = useState([]);
+  let filtered = applyFilters();
   let total = filtered.length;
   let totalMessage = `Listing ${total} items`;
+  
+  function applyFilters() {
+    const term = search.trim().toLowerCase();
 
-  function handleSearch(event) {
-    const search = event.target.value.toLowerCase();
-    if (search) {
-      console.log(search);
-      let filtered = program.filter((item) => {
-        if (item.title && item.title.toLowerCase().includes(search))
-          return true;
-        if (item.desc && item.desc.toLowerCase().includes(search)) return true;
+    // If no filters, return full program;
+    if (term.length === 0 && selLoc.length === 0) return program;
+
+    let filtered = program;
+
+    // Filter by search term.
+    if (term.length) {
+      filtered = filtered.filter((item) => {
+        if (item.title && item.title.toLowerCase().includes(term)) return true;
+        if (item.desc && item.desc.toLowerCase().includes(term)) return true;
         if (item.people) {
-          for (let person of item.people) {
-            if (person.name && person.name.toLowerCase().includes(search))
+          for (const person of item.people) {
+            if (person.name && person.name.toLowerCase().includes(term))
               return true;
           }
         }
         return false;
       });
-      setFiltered(filtered);
-      return;
     }
-    setFiltered(program);
+    // Filter by location
+    if (selLoc.length) {
+      filtered = filtered.filter((item) => {
+        for (const location of item.loc) {
+          for (const selected of selLoc) {
+            if (selected.value === location) return true;
+          }
+        }
+        return false;
+      });
+    }
+    return filtered;
+  }
+
+  function handleSearch(event) {
+    setSearch(event.target.value);
+  }
+
+  function handleLoc(value) {
+    setSelLoc(value);
   }
 
   return (
     <div>
       <div className="filter">
+        <div className="filter-locations">
+          <ReactSelect
+            placeholder="Select locations"
+            options={locations}
+            isMulti
+            value={selLoc}
+            onChange={handleLoc}
+          />
+        </div>
         <div className="filter-search">
           <input
             type="text"
             placeholder="Enter search text"
+            value={search}
             onChange={handleSearch}
           />
         </div>
