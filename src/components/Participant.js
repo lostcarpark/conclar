24b9698182366dd9f9f81d982f67d5113e69a7d2
James Shields <james@lostcarpark.com>
2022-01-10 02:31:03 +0000

    Added Show Thumbnails to People.

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const Participant = ({ person }) => {
+const Participant = ({ person, thumbnails }) => {
   let img =
-    person.links && person.links.img ? (
-      <img src={person.links.img} alt={person.name} />
+    (thumbnails && person.links && person.links.img) ? (
+      <div className="participant-image"><img src={person.links.img} alt={person.name} /></div>
     ) : (
       ""
     );
   return (
     <li className="participant">
       <Link to={"/people/" + person.id}>
-        <div>{img}</div>
+        {img}
         <span>{person.name}</span>
       </Link>
     </li>
   );
+};
+
+Participant.defaultProps = {
+  thumbnails: true,
+};
+
+Participant.propTypes = {
+  thumbnails: PropTypes.bool,
 };
 
 export default Participant;
--- src/components/People.js
+++ src/components/People.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Participant from "./Participant";
 
 const People = ({ people }) => {
+  const storedThumbnails = localStorage.getItem('thumbnails'); // Get default thumbnails from local storage.
+  const [thumbnails, setThimbnails] = useState(storedThumbnails === 'false' ? false : true); // Default to true unless false explicitly stored.
+  //console.log(people);
   const rows = [];
   people.forEach((person) => {
-    rows.push(<Participant key={person.id} person={person} />);
+    rows.push(<Participant key={person.id} person={person} thumbnails={thumbnails} />);
   });
+
+  function handleThumbnail(event) {
+    setThimbnails(event.target.checked);
+    localStorage.setItem('thumbnails', event.target.checked ? 'true' : 'false');
+  }
+
   return (
     <div className="people">
+      <div className="people-settings">
+        <input id="thumbnails" name="thumbnails" type="checkbox" checked={thumbnails} onChange={handleThumbnail} />
+        <label htmlFor="thumbnails">Show thumbnails</label>
+      </div>
       <ul>{rows}</ul>
     </div>
   );
