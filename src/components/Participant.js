02b5c1f4730474baecdc58416f175802027a0482
Leane Verhulst <lverhulst@gmail.com>
2022-01-17 22:26:44 -0600

    Fix participants.js bug - missing configData value

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import configData from "../config.json";
 
 const Participant = ({ person, thumbnails }) => {
   function getParticipantThumbnail(person) {
