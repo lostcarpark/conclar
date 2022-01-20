3e590db3dcb20f446e9eb0808b976dab7000d950
James Shields <james@lostcarpark.com>
2022-01-20 02:08:11 +0000

    Refactor App from class to function with useEffect and useState, to enable useContext to be added. Changed Info component to only need one property.

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,13 +2,11 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
 import configData from "../config.json";
 
-const Info = ({ info, infoIsLoaded }) => {
-  const renderedInfo = infoIsLoaded ? (
-    <ReactMarkdown children={info} remarkPlugins={[remarkGfm]} />
-  ) : (
-    configData.INFORMATION.LOADING_MESSAGE
-  );
-  return <div className="info">{renderedInfo}</div>;
+const Info = ({ info }) => {
+  if (info === null) {
+    return <div className="info info-loading">{configData.INFORMATION.LOADING_MESSAGE}</div>;
+  }
+  return <div className="info"><ReactMarkdown children={info} remarkPlugins={[remarkGfm]} /></div>;
 };
 
 export default Info;
