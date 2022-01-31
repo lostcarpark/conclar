1975a17b474130d21e3a7b77f46e122a5620940d
M. C. DeMarco <mcd@mcdemarco.net>
2022-01-31 13:47:25 -0500

    Allow DOMPurify options

--- src/components/Person.js
+++ src/components/Person.js
@@ -22,7 +22,7 @@ const Person = () => {
     ) : (
       ""
     );
-  const safeBio = person.bio ? DOMPurify.sanitize(person.bio) : "";
+  const safeBio = person.bio ? DOMPurify.sanitize(person.bio, JSON.parse(JSON.stringify(configData.PEOPLE.BIO.PURIFY_OPTIONS))) : "";
   return (
     <div className="person">
       <h2 className="person-name">
