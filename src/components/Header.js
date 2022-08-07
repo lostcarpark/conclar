664f5e5b04000f6ba747241da7921b4f09e9bf8e
Leane Verhulst <lverhulst@gmail.com>
2022-08-07 16:29:52 -0500

    Update header, config and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import configData from "../config.json";
+
+const headerImg = configData.HEADER.IMG_SRC ? <img src={configData.HEADER.IMG_SRC} alt={configData.HEADER.IMG_ALT_TEXT}></img> : "";
+const showBreak = configData.HEADER.LINEFEED_AFTER_URL ? <br /> : "";
 
 const Header = ({ title }) => {
   document.title = title;
   return (
     <header>
+      {headerImg}
+      {showBreak}
       <h1>{title}</h1>
     </header>
   );
