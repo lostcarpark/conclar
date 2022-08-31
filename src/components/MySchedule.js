ab27a6b9114c8cad66764c0fd919f33b2c97fbfe
lostcarpark <james@lostcarpark.com>
2022-09-01 00:18:43 +0100

    Add expand all/collapse all to My Schedule.

--- src/components/MySchedule.js
+++ src/components/MySchedule.js
@@ -1,4 +1,4 @@
-import { useStoreState } from "easy-peasy";
+import { useStoreState, useStoreActions } from "easy-peasy";
 import configData from "../config.json";
 import ProgramList from "./ProgramList";
 import ShowPastItems from "./ShowPastItems";
@@ -9,6 +9,12 @@ const MySchedule = () => {
   const mySchedule = useStoreState((state) => state.getMySchedule);
   const program = useStoreState((state) => state.program);
   const showPastItems = useStoreState((state) => state.showPastItems);
+  const { expandSelected, collapseSelected } = useStoreActions((actions) => ({
+    expandSelected: actions.expandSelected,
+    collapseSelected: actions.collapseSelected,
+  }));
+  const noneExpanded = useStoreState((state) => state.noneExpanded);
+  const allSelectedExpanded = useStoreState((state) => state.allSelectedExpanded);
 
   const filtered =
     LocalTime.isDuringCon(program) && !showPastItems
@@ -21,6 +27,16 @@ const MySchedule = () => {
         <h2>{configData.PROGRAM.MY_SCHEDULE.TITLE}</h2>
       </div>
       <div className="result-filters">
+        <div className="stack">
+          <div className="filter-expand">
+            <button disabled={allSelectedExpanded} onClick={expandSelected}>
+              {configData.EXPAND.EXPAND_ALL_LABEL}
+            </button>
+            <button disabled={noneExpanded} onClick={collapseSelected}>
+              {configData.EXPAND.COLLAPSE_ALL_LABEL}
+            </button>
+          </div>
+        </div>
         <div className="filter-options">
           <ShowPastItems />
         </div>
