3615bf0b77ac366f46d4becf0e8c60db99b96072
James Shields <james@lostcarpark.com>
2024-07-28 08:53:26 +0100

    Push footer to bottom.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import PropTypes from "prop-types";
 import configData from "../config.json";
+import Navigation from "./Navigation";
 
 const headerImg = configData.HEADER.IMG_SRC ? <img src={configData.HEADER.IMG_SRC} alt={configData.HEADER.IMG_ALT_TEXT}></img> : "";
 const showBreak = configData.HEADER.LINEFEED_AFTER_URL ? <br /> : "";
 
-const Header = ({ title }) => {
+const Header = ({ title, showNavigation = true }) => {
   document.title = title;
   return (
     <header>
       {headerImg}
       {showBreak}
       <h1>{title}</h1>
+      { showNavigation ? <Navigation /> : <></> }
     </header>
   );
 };
