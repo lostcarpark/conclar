aa2daea81f05eb7e424dea21d33982802d99ad79
James Shields <james@lostcarpark.com>
2024-07-28 09:39:54 +0100

    Fix problem with invalid person in program item.

--- src/ProgramData.js
+++ src/ProgramData.js
@@ -115,8 +115,10 @@ export class ProgramData {
     // Add extra participant info to program participants.
     for (let item of program) {
       if (item.people) {
+        console.log(item.id, item.title);
         // Loop through people backwards, so we don't miss anyone if entries are removed.
         for (let index = item.people.length - 1; index >= 0; index--) {
+          console.log(index);
           let fullPerson = people.find(
             (fullPerson) => fullPerson.id === item.people[index].id
           );
@@ -124,8 +126,10 @@ export class ProgramData {
             item.people.splice(index, 1);
           }
           //Moderator check before nuking the item person data.
+          console.log(index, item.people.length, item.people[index]);
           if (
             item.people.length > 0 &&
+            typeof(item.people[index]) !== 'undefined' &&
             ((item.people[index].hasOwnProperty("name") &&
               item.people[index].name.indexOf("(moderator)") > 0) ||
             (item.people[index].hasOwnProperty("role") &&
