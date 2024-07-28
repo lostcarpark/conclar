f7ab39f3f6955d98ef95c648020231d51d2d66c4
James Shields <james@lostcarpark.com>
2024-07-28 09:44:47 +0100

    Forgot to remove debug lines.

--- src/ProgramData.js
+++ src/ProgramData.js
@@ -115,10 +115,8 @@ export class ProgramData {
     // Add extra participant info to program participants.
     for (let item of program) {
       if (item.people) {
-        console.log(item.id, item.title);
         // Loop through people backwards, so we don't miss anyone if entries are removed.
         for (let index = item.people.length - 1; index >= 0; index--) {
-          console.log(index);
           let fullPerson = people.find(
             (fullPerson) => fullPerson.id === item.people[index].id
           );
@@ -126,7 +124,6 @@ export class ProgramData {
             item.people.splice(index, 1);
           }
           //Moderator check before nuking the item person data.
-          console.log(index, item.people.length, item.people[index]);
           if (
             item.people.length > 0 &&
             typeof(item.people[index]) !== 'undefined' &&
