6daf418edc4d7891815c0b42e5aea93d19c64e9d
James Shields <james@lostcarpark.com>
2026-08-27 20:04:53 +0100

    Fix missing code from cherry-pick.

--- src/ProgramData.js
+++ src/ProgramData.js
@@ -276,24 +276,28 @@ export class ProgramData {
       if (hasProps) {
         if (tag.hasOwnProperty("label")) newTag.label = tag.label;
         if (tag.hasOwnProperty("category")) newTag.category = tag.category;
+        newTag.weight = tagWeight(tag, newTag.value);
         tags.all[value] = newTag;
         return newTag;
       }
       // If we get here, it's an old style tag.
+      const PREFIX_INDEX = 1;
+      const LABEL_INDEX = 2;
       const matches = tag.match(/^(.+):(.+)/);
-      if (matches && matches.length === 3) {
-        const prefix = matches[1];
-        const label = matches[2];
+      const hasPrefix = matches && matches.length === 3;
+      const lookupValue = hasPrefix ? matches[LABEL_INDEX] : tag;
+      if (hasPrefix) {
         // Tag has a prefix. Check if it's one we're interested in.
-        if (prefix in tags) {
-          newTag.category = prefix;
-          newTag.label = Format.formatTag(label);
+        if (matches[PREFIX_INDEX] in tags) {
+          newTag.category = matches[PREFIX_INDEX];
+          newTag.label = Format.formatTag(matches[LABEL_INDEX]);
         } else {
           newTag.label = tag;
         }
       } else {
         newTag.label = tag;
       }
+      newTag.weight = tagWeight(tag, lookupValue);
       tags.all[value] = newTag;
       return newTag;
     }
